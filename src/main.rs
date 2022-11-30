9cd5e8b28b5278aef2af6cd07e12848a0057e9c4
a <a>
2022-11-30 16:20:32 +0900

    flow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#![allow(unused)]
 fn main() {
-    println!("Hello, world!");
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+let five = Some(5);
+println!("{:?}",five);
+let six = plus_one(five);
+println!("{:?} should be 6",six);
+assert_eq!(six.unwrap_or(6),6);
+assert_eq!(six.map(|v| format!("value is {}", v)), Some("value is 1".to_string()));
+let none = plus_one(None);
 }
