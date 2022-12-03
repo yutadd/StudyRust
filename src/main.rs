ef36b93ffa24541b83eb94bbb9273330a233e216
yutadd <spyk4m49@gmail.com>
2022-12-03 16:02:25 +0900

    update file structure

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-mod easy_another_file;
-mod easy_do_match;
-mod normal_web_server;
-mod simple_using_thread_pool;
+mod lv1_another_file;
+mod lv1_do_match;
+mod lv3_web_server;
+mod lv2_using_thread_pool;
 use std::io;
 use std::thread;
 /*@author 坂島悠太(DOTPIANO_DEV)
@@ -16,12 +16,12 @@ fn text_input(){
 }
 
 fn main() {
-    easy_another_file::init(&mut "not using pool".to_string());
+    lv1_another_file::init(&mut "not using pool".to_string());
     /*This section does not use a thread pool so if this process is in loop this will overflow some day.*/
     let input_text_handle=thread::spawn(move || {text_input()});
-    simple_using_thread_pool::init();
-    easy_do_match::init();
+    lv2_using_thread_pool::init();
+    lv2_do_match::init();
     
-    normal_web_server::start();
+    lv3_web_server::start();
     input_text_handle.join().expect("Thread join failed");
 }
